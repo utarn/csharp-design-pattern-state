cf1d4f6f7de42587329c2f5676b49dd840a33bd9
Utarn <utarn@yahoo.com>
2020-04-11 08:55:25 +0700

    Project starter

---
+++ Canvas.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace pattern_state
+{
+    public class Canvas
+    {
+        public void mouseDown()
+        {
+
+        }
+
+        public void mouseUp()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace pattern_state
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newCanvas = new Canvas();
+            newCanvas.mouseDown();
+            newCanvas.mouseUp();
+        }
+    }
+}
